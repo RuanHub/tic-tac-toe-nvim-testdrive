92cd5f561f2da6b3f3ac96ecd3289618aa137f35
RuanHub <ruanvanvuuren9@gmail.com>
2024-08-27 22:39:25 +0400

    Move Board into Class

--- headers/Board.h
+++ headers/Board.h
@@ -3,4 +3,11 @@
 class Board {
 public:
   Board();
+  void renderBoard() const;
+  void makeMove(char piece, int x, int y);
+  char getCharacterAtPos(int x, int y) const;
+  ~Board();
+
+private:
+  char positions[9];
 };
--- src/Board.cpp
+++ src/Board.cpp
@@ -1,4 +1,38 @@
 #include "Board.h"
 #include <iostream>
 
-Board::Board() { std::cout << "Board has been made" << std::endl; }
+Board::Board() : positions{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '} {};
+
+void Board::renderBoard() const {
+  std::cout << "+-+-+-+" << std::endl;
+  for (int i = 0; i < 3; i++) {
+    std::cout << '|';
+    for (int j = 0; j < 3; j++) {
+      std::cout << positions[j + i * 3] << '|';
+    }
+    std::cout << std::endl << "+-+-+-+" << std::endl;
+  }
+}
+
+void Board::makeMove(char piece, int x, int y) {
+  if (x * 3 + y >= 9) {
+    std::cout << "Input out of range" << std::endl;
+    return;
+  }
+
+  if (getCharacterAtPos(x, y) != ' ') {
+    std::cout << "Position already occupied!" << std::endl;
+  }
+
+  positions[x * 3 + y] = piece;
+}
+
+char Board::getCharacterAtPos(int x, int y) const {
+  if (x * 3 + y >= 9) {
+    std::cout << "Input out of range" << std::endl;
+    return NULL;
+  }
+
+  return positions[x * 3 + y];
+}
+Board::~Board() {};
